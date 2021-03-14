06f0919c34feafecc59b4554c9bdb083cad6ab23
forge1010 <ale.silva1120@gmail.com>
2021-03-14 17:45:36 -0300

    forech finalizado

--- modules/quartos/listar.php
+++ modules/quartos/listar.php
@@ -7,6 +7,19 @@ $quartos[] = array("nome" => "Quarto Triplo", "quantidade_pessoas" => 3, "custo"
 $quartos[] = array("nome" => "Quarto Familia", "quantidade_pessoas" => 5, "custo" => "$640,00", "cama" => 5, "tamanho" => "64m");
 $quartos[] = array("nome" => "Quarto Compartilhado", "quantidade_pessoas" => 12, "custo" => "$100,00", "cama" => 12, "tamanho" => "70m");
 
+foreach($quartos as $quarto){
+    echo '<br/>';
+    echo '<br/>';
+    echo 'Nome do quarto ' .$quarto['nome']  ;
+    echo '<br/>';
+    echo 'Quantidades de pessoas' .$quarto['quantidade_pessoas'] ;
+    echo '<br/>';
+    echo 'Preço' .$quarto['custo'] ;
+    echo '<br/>';
+    echo 'Quantidade de camas disponiveis :' .$quarto['cama']  ;
+    echo '<br/>';
+    echo 'tamanho :' .$quarto['tamanho']  ;
 
+}
 
 ?>
--- modules/suporte/listar.php
+++ modules/suporte/listar.php
@@ -1,18 +1,32 @@
 <?php 
  $suporte = array(
-     array("cliente"=>"Hotel Correia", "plano"=>"gratuito", "horarioDeSuporte"=>"horario comercial", "atendenteExclusivo"=> false,
+ $suporte[]= array("cliente"=>"Hotel Correia", "plano"=>"gratuito", "horarioDeSuporte"=>"horario comercial", "atendenteExclusivo"=> false,
      "siteExclusivo"=>false ),
-     array("cliente"=>"Hotel Rocha", "plano"=>"pago", "horarioDeSuporte"=>"24h", "atendenteExclusivo"=> true,
+ $suporte[]= array("cliente"=>"Hotel Rocha", "plano"=>"pago", "horarioDeSuporte"=>"24h", "atendenteExclusivo"=> true,
      "siteExclusivo"=>true ),
-     array("cliente"=>"Hotel Azevedo", "plano"=>"pago", "horarioDeSuporte"=>"24h", "atendenteExclusivo"=> true,
+ $suporte[]= array("cliente"=>"Hotel Azevedo", "plano"=>"pago", "horarioDeSuporte"=>"24h", "atendenteExclusivo"=> true,
      "siteExclusivo"=>false ),
-     array("cliente"=>"Hotel Macapá", "plano"=>"gratuito", "horarioDeSuporte"=>"horario comercial", "atendenteExclusivo"=> false,
+ $suporte[]= array("cliente"=>"Hotel Macapá", "plano"=>"gratuito", "horarioDeSuporte"=>"horario comercial", "atendenteExclusivo"=> false,
      "siteExclusivo"=>false ),
-     array("cliente"=>"Hotel Santana", "plano"=>"gratuito", "horarioDeSuporte"=>"horario comercial", "atendenteExclusivo"=> false,
+ $suporte[]= array("cliente"=>"Hotel Santana", "plano"=>"gratuito", "horarioDeSuporte"=>"horario comercial", "atendenteExclusivo"=> false,
      "siteExclusivo"=>true ),
- )
+ );
 
+ foreach($suporte as $sup){
+    echo '<br/>';
+    echo '<br/>';
+    echo 'Cliente ' .$sup['nome']  ;
+    echo '<br/>';
+    echo 'Tipo de plano: ' .$sup['plano'] ;
+    echo '<br/>';
+    echo 'Horario de suporte ' .$sup['horarioDeSuporte'] ;
+    echo '<br/>';
+    echo ($sup["atendenteExclusivo"]) ? " Sim" : "Não";
+    echo '<br/>';
+    echo 'Cafe da manhã gratuito'. ($sup["siteExclusivo"]) ? " Sim" : "Não";
+ }
 
+ 
 
 
 ?>
